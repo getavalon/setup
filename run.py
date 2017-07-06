37764d451d25b043c74d8bf5556e51a44e710008
Marcus Ottosson <konstruktion@gmail.com>
2017-07-06 13:53:44 +0100

    Implement forwarding API

--- run.py
+++ run.py
@@ -53,19 +53,8 @@ def install():
 
     if missing_dependencies:
         print("Sorry, there are some dependencies missing from your system.\n")
-        print("\n".join(" - %s" % d for d in missing_dependencies))
-
-    missing_environment = list()
-    for variable in ("AVALON_MONGO",):
-        if variable not in os.environ:
-            missing_environment.append(variable)
-
-    if missing_environment:
-        print("Don't forget to set the required environment variables.")
-        print("\n".join("- %s" % v for v in missing_environment))
-
-    if missing_dependencies or missing_environment:
-        print("\nSee https://getavalon.github.io/2.0/howto/#install "
+        print("\n".join(" - %s" % d for d in missing_dependencies) + "\n")
+        print("See https://getavalon.github.io/2.0/howto/#install "
               "for more details.")
         sys.exit(1)
 
@@ -73,7 +62,8 @@ def install():
     for dependency, name in (("PYBLISH_BASE", "pyblish-base"),
                              ("PYBLISH_QML", "pyblish-qml"),
                              ("AVALON_CORE", "avalon-core"),
-                             ("AVALON_LAUNCHER", "avalon-launcher")):
+                             ("AVALON_LAUNCHER", "avalon-launcher"),
+                             ("AVALON_EXAMPLES", "avalon-examples")):
         if dependency not in os.environ:
             os.environ[dependency] = os.path.join(REPO_DIR, "git", name)
 
@@ -109,7 +99,7 @@ def install():
     try:
         root = os.environ["AVALON_PROJECTS"]
     except KeyError:
-        root = os.path.join(REPO_DIR, "git", "avalon-examples", "projects")
+        root = os.path.join(os.environ["AVALON_EXAMPLES"], "projects")
         os.environ["AVALON_PROJECTS"] = root
 
     try:
@@ -123,7 +113,7 @@ def install():
         sys.exit(1)
 
 
-def forward(args, silent=False):
+def forward(args, silent=True):
     """Pass `args` to the Avalon CLI, within the Avalon Setup environment
 
     Arguments:
@@ -136,7 +126,7 @@ def forward(args, silent=False):
         print("avalon.py: Forwarding %s.." % " ".join(args))
 
     popen = subprocess.Popen(
-        [sys.executable, "-u", "-m"] + args,
+        args,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         universal_newlines=True
@@ -144,7 +134,7 @@ def forward(args, silent=False):
 
     # Blocks until finished
     for line in iter(popen.stdout.readline, ""):
-        if not silent:
+        if not silent or AVALON_DEBUG:
             sys.stdout.write(line)
 
     if AVALON_DEBUG:
@@ -157,39 +147,35 @@ def forward(args, silent=False):
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--build", action="store_true",
-                        help="Build project at the current working directory")
+    parser = argparse.ArgumentParser(usage=__doc__)
     parser.add_argument("--load", action="store_true",
                         help="Load project at the current working directory")
     parser.add_argument("--save", action="store_true",
                         help="Save project from the current working directory")
-    parser.add_argument("--show", action="store_true",
-                        help="Show the Avalon Launcher")
+    parser.add_argument("--forward",
+                        help="Run arbitrary command from setup environment")
 
     kwargs = parser.parse_args()
 
     install()
 
-    if kwargs.build:
-        # Build example projects
-        os.chdir(os.path.join(os.environ["AVALON_PROJECTS"], "batman"))
-
-        if forward(["avalon.inventory", "--save"], silent=False) == 0:
-            sys.exit(forward(["avalon.build"]))
-        else:
-            print("Couldn't save, this is a bug")
-            sys.exit(1)
-
-    elif kwargs.load:
-        sys.exit(forward(["avalon.inventory", "--load"]))
+    if kwargs.load:
+        returncode = forward([
+            sys.executable, "-u", "-m",
+            "avalon.inventory", "--load"])
 
     elif kwargs.save:
-        sys.exit(forward(["avalon.inventory", "--save"]))
+        returncode = forward([
+            sys.executable, "-u", "-m",
+            "avalon.inventory", "--save"])
 
-    elif kwargs.show:
-        root = os.environ["AVALON_PROJECTS"]
-        sys.exit(forward(["launcher", "--root", root]))
+    elif kwargs.forward:
+        returncode = forward(kwargs.forward.split(), silent=False)
 
     else:
-        print(__doc__)
+        root = os.environ["AVALON_PROJECTS"]
+        returncode = forward([
+            sys.executable, "-u", "-m",
+            "launcher", "--root", root])
+
+    sys.exit(returncode)
